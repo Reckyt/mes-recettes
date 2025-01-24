b820ed8968e55672c0dee90e6962ea1f8cefe1f4
Maxime Lombardo <maxime.lombardo@gmail.com>
2025-01-24 20:39:05 +0100

    header added with logo and title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [HeaderComponent],
 })
-export class AppComponent {
-  title = 'Mes recettes';
-}
+export class AppComponent {}
